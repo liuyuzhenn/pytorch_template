e491ed739e9eb32f6fa2b45b76a22e9f3bf34847
liuyuzhen <237925240@qq.com>
2023-01-31 05:53:42 +0800

    add recursive function wrapper

--- torch_temp/trainers/utils.py
+++ torch_temp/trainers/utils.py
@@ -2,66 +2,58 @@ import numpy as np
 import torch
 import torchvision.utils as vutils
 
-def make_recursive_func(func):
-    def wrapper(vars):
-        if isinstance(vars, list):
-            return [wrapper(x) for x in vars]
-        elif isinstance(vars, tuple):
-            return tuple([wrapper(x) for x in vars])
-        elif isinstance(vars, dict):
-            return {k: wrapper(v) for k, v in vars.items()}
+
+def recursive(func):
+    def wrapper(*args, **kwargs):
+        if isinstance(args[0], list):
+            return [wrapper(*[x, *args[1:]], **kwargs) for x in args[0]]
+        elif isinstance(args[0], tuple):
+            return tuple([wrapper(*[x, *args[1:]], **kwargs) for x in args[0]])
+        elif isinstance(args[0], dict):
+            return {k: wrapper(*[v, *args[1:]], **kwargs) for k, v in args[0].items()}
         else:
-            return func(vars)
+            return func(*args, **kwargs)
     return wrapper
 
-def make_recursive_func_arg(func):
-    def wrapper(vars,args):
-        if isinstance(vars, list):
-            return [wrapper(x,args) for x in vars]
-        elif isinstance(vars, tuple):
-            return tuple([wrapper(x,args) for x in vars])
-        elif isinstance(vars, dict):
-            return {k: wrapper(v,args) for k, v in vars.items()}
-        else:
-            return func(vars,args)
-    return wrapper
 
-@make_recursive_func_arg
-def to_device(sample,device):
-    if device=='cpu':
-        if isinstance(sample,torch.Tensor):
+@recursive
+def to_device(sample, device):
+    if device == 'cpu':
+        if isinstance(sample, torch.Tensor):
             return sample.cpu()
         else:
             return sample
-    elif device=='cuda':
-        if isinstance(sample,torch.Tensor):
+    elif device == 'cuda':
+        if isinstance(sample, torch.Tensor):
             return sample.cuda()
         else:
             return sample
     else:
         return sample.to(device)
-    
 
-        
-@make_recursive_func
+
+@recursive
 def tensor2float(vars):
     if isinstance(vars, float):
         return vars
     elif isinstance(vars, torch.Tensor):
         return vars.data.item()
     else:
-        raise NotImplementedError("invalid input type {} for tensor2float".format(type(vars)))
+        raise NotImplementedError(
+            "invalid input type {} for tensor2float".format(type(vars)))
 
 
-@make_recursive_func
+@recursive
 def tensor2numpy(vars):
     if isinstance(vars, np.ndarray):
         return vars
     elif isinstance(vars, torch.Tensor):
         return vars.detach().cpu().numpy().copy()
     else:
-        raise NotImplementedError("invalid input type {} for tensor2numpy".format(type(vars)))
-        
+        raise NotImplementedError(
+            "invalid input type {} for tensor2numpy".format(type(vars)))
+
+
 def save_scalars(writer, mode, scalar_dict, global_step):
     scalar_dict = tensor2float(scalar_dict)
     for key, value in scalar_dict.items():
@@ -79,7 +71,8 @@ def save_images(writer, mode, images_dict, global_step):
 
     def preprocess(name, img):
         if not (len(img.shape) == 3 or len(img.shape) == 4):
-            raise NotImplementedError("invalid img shape {}:{} in save_images".format(name, img.shape))
+            raise NotImplementedError(
+                "invalid img shape {}:{} in save_images".format(name, img.shape))
         if len(img.shape) == 3:
             img = img[:, np.newaxis, :, :]
         img = torch.from_numpy(img[:1])
@@ -92,14 +85,16 @@ def save_images(writer, mode, images_dict, global_step):
         else:
             for idx in range(len(value)):
                 name = '{}/{}_{}'.format(mode, key, idx)
-                writer.add_image(name, preprocess(name, value[idx]), global_step)
+                writer.add_image(name, preprocess(
+                    name, value[idx]), global_step)
+
 
 def dict_to_str(d, sep='| '):
     out = []
-    for k,v in d.items():
-        out.append('{}: {} '.format(k,v))
+    for k, v in d.items():
+        out.append('{}: {} '.format(k, v))
     return sep.join(out)
-    
+
 
 class DictAverageMeter(object):
     def __init__(self):
@@ -111,29 +106,18 @@ class DictAverageMeter(object):
         if len(self.data) == 0:
             for k, v in new_input.items():
                 if not isinstance(v, float):
-                    raise NotImplementedError("invalid data {}: {}".format(k, type(v)))
+                    raise NotImplementedError(
+                        "Invalid data {}: {}".format(k, type(v)))
                 self.data[k] = v
         else:
             for k, v in new_input.items():
                 if not isinstance(v, float):
-                    raise NotImplementedError("invalid data {}: {}".format(k, type(v)))
+                    raise NotImplementedError(
+                        "Invalid data {}: {}".format(k, type(v)))
                 self.data[k] += v
 
     def mean(self):
-        if self.count>0:
+        if self.count > 0:
             return {k: v / self.count for k, v in self.data.items()}
         else:
             return {}
-
-# a wrapper to compute metrics for each image individually
-def compute_metrics_for_each_image(metric_func):
-    def wrapper(depth_est, depth_gt, mask, *args):
-        batch_size = depth_gt.shape[0]
-        results = []
-        # compute result one by one
-        for idx in range(batch_size):
-            ret = metric_func(depth_est[idx], depth_gt[idx], mask[idx], *args)
-            results.append(ret)
-        return torch.stack(results).mean()
-
-    return wrapper
