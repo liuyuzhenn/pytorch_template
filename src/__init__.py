6aeaf4b9f7890189ebef56c9738b5e25eb5da9a4
liuyuzhen <237925240@qq.com>
2024-04-28 21:22:27 +0800

    add function

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,7 @@ def load_configs(path):
     with open(path, 'r') as f:
         configs = yaml.full_load(f)
     return configs
+
+def save_configs(path, mdict):
+    with open(path, 'w') as f:
+        yaml.dump(mdict, f, default_style=False, sort_keys=False)
