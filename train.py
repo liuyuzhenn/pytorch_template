3ee7486a4623cc102974effccb653d5b60805033
liuyuzhen <237925240@qq.com>
2024-05-17 15:01:42 +0800

    dist train memory bug

--- train.py
+++ train.py
@@ -37,6 +37,7 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 
 if int(os.environ.get('LOCAL_RANK', -1)) >= 0:
+    torch.cuda.set_device(int(os.environ.get('LOCAL_RANK', -1)))
     torch.distributed.init_process_group('nccl', init_method='env://')
 
 ttrain(configs)
